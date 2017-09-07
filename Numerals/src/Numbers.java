84dd158938e92990a401bd48d316ddf3d19b2f76
gabbley <baniquedg@bishopireton.org>
2017-09-07 10:20:05 -0400

    Test work in class

---
+++ Numerals/src/Numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author baniquedg
+ *
+ */
+public class Numbers {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter 2 numbers:");
+		
+		int x = keyboard.nextInt();
+		int y = keyboard.nextInt();
+		
+		System.out.printf("Adding %d and %d results in %d\n", x, y, (x+y));
+		keyboard.close();
+		System.out.println("Test Change");
+		System.out.println("yeah");
+	}
+
+}
