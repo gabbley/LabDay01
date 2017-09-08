2af919b11db8a1480c39f2e47d5fb6885bc7032b
MurphyP1 <MurphyP1@104-10-2017.bi.local>
2017-09-08 11:21:47 -0400

    Made some changes, idk if it works bc of the weird ant build
    I think you infected my eclipse now too

--- Numerals/src/Numbers.java
+++ Numerals/src/Numbers.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
  */
 
 /**
- * @author baniquedg
+ * @author MurphyPOne
+ * weird issue with the Ant build...
  *
  */
 public class Numbers {
@@ -19,10 +20,11 @@ public class Numbers {
 		int x = keyboard.nextInt();
 		int y = keyboard.nextInt();
 		System.out.printf("SUM: %d + %d = %d\n", x, y, sum(x,y) +
-				"DIFFERENCE: %d - %d = %d\n ", x, y, sum(x,y) +
-				"PRODUCT: %d * %d = %d\n ", x, y, sum(x,y) + 
-				"QUOTIENT: %d / %d = %d\n ", x, y, sum(x,y) +
-				"MODULUS: %d % %d = %d\n ", x, y, sum(x,y)
+				"DIFFERENCE: %d - %d = %d\n ", x, y, diff(x,y) +
+				"PRODUCT: %d * %d = %d\n ", x, y, prod(x,y) + 
+				"QUOTIENT: %d / %d = %d\n ", x, y, quot(x,y) +
+				"MODULUS: %d % %d = %d\n ", x, y, mod(x,y) + 
+				"REVMODULUS: %d % %d = %d\n ", x, y, revmod(x,y)	//Added this bad boy to the print statement
 				);
 		keyboard.close();
 		
@@ -44,8 +46,14 @@ public class Numbers {
 		return x/y;
 	}
 	
-	public int mod(int x, int x){
+	//Fixed some of the parameters 
+	public int mod(int x, int y){
 		return x%y;
+	}
+	
+	//New method which really doesn't do much
+	public int revmod(int x, int y){
+		return y%x;
 	}
 	
 	
